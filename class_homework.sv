45f91f36e7f5b9bee744e36baa069d48b4cdfc23
bumjoooon <kbj970505@naver.com>
2023-09-25 14:19:38 +0900

    problem 3 complete

--- class_homework.sv
+++ class_homework.sv
@@ -1,39 +1,43 @@
 module exchange_module;
    
+   parameter real Euro_Rate = 0.93871;
+   parameter real Won_Rate = 1335.9;
+   parameter real Yen_Rate = 148.29;
+
    function get_Euro_from_Dollar(real amount);
       real euro;
-      euro = 0.93871 * amount;
+      euro = Euro_Rate * amount;
       $display("Dollar %f = Euro %f", amount, euro);
    endfunction
 
    function get_Dollar_from_Euro(real amount);
       real dollar;
-      dollar = amount / 0.93871;
+      dollar = amount / Euro_Rate;
     $display("Euro %f = Dollar %f", amount, dollar);
    endfunction
 
 
    function get_Won_from_Dollar(real amount);
       real won;
-      won = amount * 1335.9;
+      won = amount * Won_Rate;
     $display("Dollar %f = Won %f", amount, won);
    endfunction
 
    function get_Dollar_from_Won(real amount);
       real dollar;
-      dollar = amount / 1335.9;
+      dollar = amount / Won_Rate;
     $display("Won %f = Dollar %f", amount, dollar);
    endfunction
 
    function get_Yen_from_Dollar(real amount);
       real yen;
-      yen = amount / 148.29;
+      yen = amount * Yen_Rate;
     $display("Dollar %f = Yen %f", amount, yen);
    endfunction
 
    function get_Dollar_from_Yen(real amount);
       real dollar;
-      dollar = amount * 148.29;
+      dollar = amount / Yen_Rate;
      $display("Yen %f = Dollar %f", amount, dollar);
    endfunction
 
