5b2c74c4b0d08b84f61ca66e9fa3c02da5eded46
bumjoooon <kbj970505@naver.com>
2023-09-26 10:22:39 +0900

    생산성 업그레이드  완료

--- class_homework_revise.sv
+++ class_homework_revise.sv
@@ -4,70 +4,80 @@ module exchange_module;
    parameter real Won_Rate = 1335.9;
    parameter real Yen_Rate = 148.29;
 
-  
    class GetFrom;
      real amount;          //환전 '전' 화폐 '액수'
      string from;          //환전 '전' 화폐 '이름'
      real get_money;       //환전 '후' 화폐 '액수'
      string get;           //환전 '후' 화폐 '이름'
 
-     function new (string from,real amount, string get, real get_money);
-        this.from = from;
-        this.amount = amount;
-        this.get = get;
-        this.get_money = get_money;
+     function new (real amount);
+        this.amount = amount;	//환전 전 액수
      endfunction
-
-     function display();
-         $display("%s : %f\n%s : %f\n", this.from, this.amount, this.get, this.get_money);
-     endfunction
-   endclass
-
-  
-  
-   class gEfD extends GetFrom;
      
-      string from = "Dollar"
-      string get = "Euro"
+     function GetMoney(real get_money);
+       this.get_money = get_money;		//환전 후 액수
+     endfunction
+     
+     function Name(string get, string from);
+       this.get = get;	
+     	this.from = from;
+     endfunction
+     
+     function display();
+         $display("%s : %f\n%s : %f\n", from, this.amount, this.get, this.get_money);
+     endfunction
+   endclass
+
+  
+  
+   class gEfD extends GetFrom;      //달러에서 유로로 환전
+     
+      string from = "Dollar";
+      string get = "Euro";
+      real get_money;
 
      function new(real amount);
-        real get_money;
-        get_money = Euro_Rate * amount
-       super.new(this.from, amount, this.get, get_money);
+       super.new(amount); 
+       this.get_money =  amount * Euro_Rate;
+       super.GetMoney( get_money);
+       super.Name(get,from);
+       
        super.display();
      endfunction
 
-     // function get_Euro_from_Dollar();
-     //    real euro;
-     //    euro = Euro_Rate * this.amount;
-     //   display("Dollar",amount,"Euro",euro);
-     // endfunction
    endclass
 
 
-   class gDfE extends GetFrom;
+   class gDfE extends GetFrom;         //유로에서 달러로 환전
 
-      string from = "Euro"
-      string get = "Dollar"
+      string from = "Euro";
+      string get = "Dollar";
+      real get_money;
 
      function new(real amount);
-        real get_money;
-        get_money = Euro_Rate / amount
-       super.new(this.from, amount, this.get, get_money);
+       super.new(amount); 
+       this.get_money = amount / Euro_Rate;
+       super.GetMoney( get_money);
+       super.Name(get,from);
+       
        super.display();
      endfunction
    endclass
+
 
 
    class gWfD extends GetFrom;
 
-      string from = "Dollar"
-      string get = "Euro"
+      string from = "Dollar";
+      string get = "Won";
+     real get_money;
 
      function new(real amount);
-        real get_money;
-        get_money = Euro_Rate * amount
-       super.new(this.from, amount, this.get, get_money);
+       super.new(amount); 
+       this.get_money = amount * Won_Rate;
+       super.GetMoney( get_money);
+       super.Name(get,from);
+       
        super.display();
      endfunction
    endclass
@@ -75,26 +85,33 @@ module exchange_module;
   
    class gDfW extends GetFrom;
 
-      string from = "Dollar"
-      string get = "Euro"
+      string from = "Won";
+      string get = "Dollar";
+     real get_money;
 
-     function new(real amount);
-        real get_money;
-        get_money = Euro_Rate * amount
-       super.new(this.from, amount, this.get, get_money);
+      function new(real amount);
+       super.new(amount); 
+       this.get_money = amount / Won_Rate;
+       super.GetMoney( get_money);
+       super.Name(get,from);
+       
        super.display();
      endfunction
    endclass
 
    class gYfD extends GetFrom;
 
-      string from = "Dollar"
-      string get = "Euro"
+      string from = "Dollar";
+      string get = "Yen";
+     real get_money;
+     
 
      function new(real amount);
-        real get_money;
-        get_money = Euro_Rate * amount
-       super.new(this.from, amount, this.get, get_money);
+       super.new(amount); 
+       this.get_money = amount * Yen_Rate;
+       super.GetMoney( get_money);
+       super.Name(get,from);
+       
        super.display();
      endfunction
    endclass
@@ -102,38 +119,33 @@ module exchange_module;
 
    class gDfY extends GetFrom;
 
-      string from = "Dollar"
-      string get = "Euro"
+      string from = "Yen";
+      string get = "Dollar";
+     real get_money;
 
      function new(real amount);
-        real get_money;
-        get_money = Euro_Rate * amount
-       super.new(this.from, amount, this.get, get_money);
+       super.new(amount); 
+       this.get_money = amount / Yen_Rate;
+       super.GetMoney( get_money);
+       super.Name(get,from);
+       
        super.display();
      endfunction
-
    endclass
 
 
-
+  
    initial begin
       gEfD test1 = new(100);
-      gDfE test2 = new(20);
+     gDfE test2 = new(20);
       gWfD test3 = new(100);
-      gDfW test4 = new(20);
-      gYfD test5 = new(13000);
-      gDfY test6 = new(150000);
+     gDfW test4 = new(20);
+     gYfD test5 = new(13000);
+     gDfY test6 = new(150000);
       
      
-      test1.get_Euro_from_Dollar();
-      test2.get_Dollar_from_Euro();
-     
-      
-      test3.get_Won_from_Dollar();
-      test4.get_Dollar_from_Won();
 
-      test5.get_Yen_from_Dollar();
-      test6.get_Dollar_from_Yen();
    end
 
 endmodule
+
